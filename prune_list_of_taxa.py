4484d6a2051a6167b1ab55c548e4cfab4070671a
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2018-03-30 08:47:41 -0500

    wrote DVMC script

---
+++ prune_list_of_taxa.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import os.path
+from Bio import Phylo
+from Bio.Phylo.BaseTree import TreeMixin
+
+def prune(
+    tree, taxa
+    ):
+    """
+    prune taxa from a newick tree
+    
+    Parameters
+    ----------
+    argv: tree
+        newick tree file
+    argv: taxa
+        taxa to prune from tree
+    """
+
+    # create output file name
+    filename = tree+".pruned"
+
+    # read in tree
+    tree = Phylo.read(tree, 'newick')
+
+    # save taxa into a list
+    taxaL  = []
+    taxaL = [line.rstrip('\n') for line in open(taxa)]
+
+    # prune taxa
+    for taxon in taxaL:
+        tree.prune(taxon)
+
+    # write tree
+    Phylo.write(tree, filename, 'newick')
+
+def main(
+    argv
+    ):
+    """
+    Reads arguments 
+    """
+
+    # initialize argument variables
+    tree = ''
+    taxa = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:l:")
+    except getopt.GetoptError:
+        # error message
+        print("Error\nFor help use -h argument\n")
+        sys.exit(2)
+    # if no arguments are used print a help message
+    if len(opts) == 0:
+        # error message
+        print("\nNo arguments provided...")
+        print("For help use -h argument\n")
+        sys.exit(2)
+    
+    # test for arguments
+    for opt, arg in opts:
+        if opt == '-h':
+            # general script explanation
+            print("\nPrunes list of a taxa from a newick tree file.")
+            print("Arguments include -i <newick.tree> and -l <taxa.list>\n")
+            print("Where the -i argument specifies a tree in newick format and")
+            print("the -l option specifies a single column file with taxa to prune.")
+            sys.exit()
+        elif opt == '-i':
+            if os.path.isfile(arg):
+                tree = arg
+            else:
+                # error message
+                print("\n\nThe specified tree file does not exist.\n")
+                print("For detailed explanation use -h argument\n")
+                sys.exit()
+        elif opt == '-l':
+            if os.path.isfile(arg):
+                taxa = arg
+            else:
+                # error message
+                print("\n\nThe specified list file does not exist.\n")
+                print("For detailed explanation use -h argument\n")
+                sys.exit()
+
+    # pass to calculate_treeness function
+    prune(
+        tree, taxa
+        )
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ remove_distances_from_tree.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import os.path
+#from Bio.Phylo.BaseTree import TreeMixin
+from Bio.Phylo.BaseTree import Clade
+from Bio import Phylo
+
+def modTree(
+    tree
+    ):
+    """
+    creates numerical identifiers for internodes
+    
+    Parameters
+    ----------
+    argv: tree
+        newick tree file
+    """
+
+    # create file name 
+    filename=tree+".topology.tree"
+
+    # read in tree
+    tree = Phylo.read(tree, 'newick')
+    # set branch lengths to None
+    for i in tree.get_nonterminals():
+        i.branch_length=None
+    for i in tree.get_terminals():
+        i.branch_length=None
+
+    Phylo.write(tree, filename, 'newick')
+
+def main(
+    argv
+    ):
+    """
+    Reads arguments 
+    """
+
+    # initialize argument variables
+    tree  = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "ht:")
+    except getopt.GetoptError:
+        # error message
+        print("Error\nFor help use -h argument\n")
+        sys.exit(2)
+    # if no arguments are used print a help message
+    if len(opts) == 0:
+        # error message
+        print("\nNo arguments provided...")
+        print("For help use -h argument\n")
+        sys.exit(2)
+    
+    # test for arguments
+    for opt, arg in opts:
+        if opt == '-h':
+            # general script explanation
+            print("\nRemoves distances from a tree in newick format.")
+            sys.exit()
+        elif opt == '-t':
+            if os.path.isfile(arg):
+                tree = arg
+            else:
+                # error message
+                print("\n\nThe specified tree file does not exist.\n")
+                print("For detailed explanation use -h argument\n")
+                sys.exit()
+
+    # pass to calculate_treeness function
+    modTree(
+        tree
+        )
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
